5d430c84ce882d5a633c61ddb7dc3b8d1515e038
hiraaziz <hiraaziz1998@gmail.com>
2023-03-01 15:17:56 +0500

    Create Navbar.jsx

---
+++ src/components/navigationBar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-red-900 flex justify-evenly items-center list-none text-gray-100 w-full h-16">
+      <li>Home</li>
+      <li>About</li>
+      <li>Programs</li>
+      <li>Admissions</li>
+      <li>Academics</li>
+      <li>Life at Eman</li>
+      <li>Qec</li>
+      <li>Oric</li>
+      <li>Login</li>
+      <li>Alumni</li>
+      <li>Certificate Course</li>
+      <li>Contact</li>
+    </div>
+  );
+};
+
+export default Navbar;
