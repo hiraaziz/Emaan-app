46de2366504e26108c193d0e365f902482277558
hiraaziz <hiraaziz1998@gmail.com>
2023-03-01 15:18:05 +0500

    Create Middlebar.jsx

---
+++ src/components/middleBar/Middlebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Middlebar = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-80 flex items-center">
+        <img src={"./logo.jpeg"} alt="logo" className="w-24 h-24" />
+        <h1 className="font-bold text-center text-gray-700 tracking-wider">
+          Emaan Institute of Management and Sciences
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Middlebar;
