cf91f3be8fe45f16885df69e3bc140127c8746ff
hiraaziz <hiraaziz1998@gmail.com>
2023-03-02 20:34:38 +0500

    Create constant.js

---
+++ src/components/navigationBar/constant.js
@@ -0,0 +1,34 @@
+export const links = [
+    ["About Us", "Message", "Board of Advisor", "Board of Governerce"],
+    ["Masters of Business Administration",
+    "Bachelor of Business Administration",
+    "Bachelor of Computer Science",
+    "Associate Degree Program"],
+    [
+      "Undergraduate Program",
+      "Graduate Program",
+      "Associate Degree Program",
+      "Semester Rules",
+    ],
+    [
+      "QEC Activities",
+      "Quality Policy Statement",
+      "QEC Structure",
+      "Function of QEC",
+      "Yearly Report",
+      "Memberships",
+      "New Release",
+    ],
+    [
+      "Introduction",
+      "Research Journals",
+      "Conferences",
+      "Training & Workshop",
+      "Yearly Report",
+      "Research Project/Thesis",
+    ],
+    ["Studnet Login", "Faculty Login", "Results"],
+  ];
+
+  
+  
