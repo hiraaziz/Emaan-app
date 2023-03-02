4a2a7c85a04e2c632884f9ad860d41d5d60beedc
hiraaziz <hiraaziz1998@gmail.com>
2023-03-02 20:34:45 +0500

    Update Topheader.jsx

--- src/components/topHeader/Topheader.jsx
+++ src/components/topHeader/Topheader.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { AiOutlineInstagram, AiOutlineFacebook } from "react-icons/ai";
+import {
+  BsMessenger,
+  BsInstagram,
+  BsWhatsapp,
+  BsFillTelephoneFill,
+} from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
 import styles from "./styles.module.css";
 
 const Topheader = () => {
   return (
-    <div className="w-full h-11 bg-red-800 flex justify-evenly items-center">
-      <div className="text-gray-200 flex justify-evenly w-20">
-        <AiOutlineInstagram className={styles.icons} />
-        <AiOutlineFacebook className={styles.icons} />
+    <div className="w-full h-11 bg-maroon flex justify-evenly items-center">
+      <button className="w-32 h-7 text-maroon bg-white rounded-3xl font-bold">
+        Apply Now!
+      </button>
+      <div className="flex justify-evenly w-96 text-white list-none ">
+        <li>Certificate Courses</li>
+        <li>LMS/Portal</li>
+        <li>ORIC</li>
       </div>
-      <div className="flex justify-evenly w-28 text-white">
-        <h3>Apply</h3>
-        <h3>LMS</h3>
+      <div className="text-gray-200 flex justify-evenly w-48">
+        <FaFacebookF />
+        <BsFillTelephoneFill />
+        <BsMessenger />
+        <BsInstagram />
+        <BsWhatsapp />
       </div>
     </div>
   );
