d7700813e29fc0dc64fb38dec5ff67455c803522
hiraaziz <hiraaziz1998@gmail.com>
2023-03-02 20:34:56 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ module.exports = {
     fontFamily:{
       'monserrat': ['Montserrat', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      colors:{
+        'maroon': '#6C0404'
+      },
+      spacing:{
+        '05': '500px',
+        '400': '40%',
+        '500': '50%',
+        '600': '60%',
+        '700': '70%',
+        '800': '80%',
+        '900': '90%'
+      }
+    },
   },
   plugins: [],
 }
