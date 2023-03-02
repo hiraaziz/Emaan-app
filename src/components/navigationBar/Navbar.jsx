5dfa827ef0ba34f145d947078b6dc8b9eb630aa2
hiraaziz <hiraaziz1998@gmail.com>
2023-03-02 20:34:36 +0500

    Update Navbar.jsx

--- src/components/navigationBar/Navbar.jsx
+++ src/components/navigationBar/Navbar.jsx
@@ -1,20 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import { links } from "./constant";
+import { BiChevronDown } from "react-icons/bi";
 
 const Navbar = () => {
+  const [value, setvalue] = useState("");
+  const [aboutOpen, setAboutOpen] = useState(false);
+  const [program, setprogram] = useState(false);
+  const [department, setdepartment] = useState(false);
+  const [admission, setAdmission] = useState(false);
+  const [qec, setQec] = useState(false);
+  const [acadmics, setacadmics] = useState(false);
+  const [login, setLogin] = useState(false);
+
+  const nav = [
+    {
+      name: "About",
+      value: "about",
+      funcName: aboutOpen,
+    },
+
+    {
+      name: "Department",
+      value: "department",
+      funcName: department,
+    },
+    {
+      name: "Admission",
+      value: "admission",
+      funcName: admission,
+    },
+    {
+      name: "Acadmics",
+      value: "acadmics",
+      funcName: acadmics,
+    },
+    {
+      name: "QEC",
+      value: "qec",
+      funcName: qec,
+    },
+    {
+      name: "Login",
+      value: "login",
+      funcName: login,
+    },
+  ];
+  const toggleDropdown = (value) => {
+    setAboutOpen(false);
+    setprogram(false);
+    setdepartment(false);
+    setAdmission(false);
+    setacadmics(false);
+    setLogin(false);
+    setQec(false);
+
+    if (value == "about") setAboutOpen(!aboutOpen);
+    else if (value == "program") setprogram(!program);
+    else if (value == "department") setdepartment(!department);
+    else if (value == "admission") setAdmission(!admission);
+    else if (value == "qec") setQec(!qec);
+    else if (value == "acadmics") setacadmics(!acadmics);
+    else if (value == "login") setLogin(!login);
+  };
+
   return (
-    <div className="bg-red-900 flex justify-evenly items-center list-none text-gray-100 w-full h-16">
-      <li>Home</li>
-      <li>About</li>
-      <li>Programs</li>
-      <li>Admissions</li>
-      <li>Academics</li>
-      <li>Life at Eman</li>
-      <li>Qec</li>
-      <li>Oric</li>
-      <li>Login</li>
-      <li>Alumni</li>
-      <li>Certificate Course</li>
-      <li>Contact</li>
+    <div className="bg-maroon w-full h-16 flex justify-center">
+      <div className="flex justify-evenly items-center list-none h-full w-600 text-gray-100 font-semibold">
+        <button>Home</button>
+        {nav.map((nav, ind) => (
+          <div className="relative">
+            <button
+              onClick={() => toggleDropdown(nav.value)}
+              className="flex items-center"
+            >
+              {nav.name}
+              <span className="px-2 mt-1">
+                <BiChevronDown />
+              </span>
+            </button>
+            {nav.funcName && (
+              <div className=" absolute left-0 py-2 mt-2 w-48 bg-black text-white-100 rounded-md shadow-lg z-10">
+                {links[ind].map((link) => (
+                  <>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:text-black hover:bg-gray-100"
+                    >
+                      {link}
+                    </a>
+                    <div className="w-900 h-px bg-gray-500 flex ml-2"></div>
+                  </>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+
+        <button>Contact</button>
+        <button>Alumni</button>
+      </div>
     </div>
   );
 };
