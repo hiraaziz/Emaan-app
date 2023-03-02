0c53c7ef219c4bfb80d2f5cecd180934aa18d7ef
hiraaziz <hiraaziz1998@gmail.com>
2023-03-02 20:34:31 +0500

    Update Banner.jsx

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Crousel from "./Crousel";
 
 const Banner = () => {
-  return (
-    <div>
-      <img src={"./banner.jpg"} alt="banner" />
-    </div>
-  );
+  const images = [
+    "https://picsum.photos/800/600?random=1",
+    "https://picsum.photos/800/600?random=2",
+    "https://picsum.photos/800/600?random=3",
+  ];
+
+  return <Crousel images={images} />;
 };
 
 export default Banner;
