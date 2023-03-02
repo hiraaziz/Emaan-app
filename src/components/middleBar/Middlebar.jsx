1eb61d18eab19ce17ed9f9e0ac48181b7d3f0eaf
hiraaziz <hiraaziz1998@gmail.com>
2023-03-02 20:34:34 +0500

    Update Middlebar.jsx

--- src/components/middleBar/Middlebar.jsx
+++ src/components/middleBar/Middlebar.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Middlebar = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="w-80 flex items-center">
+      <div className="w-400 flex items-center">
         <img src={"./logo.jpeg"} alt="logo" className="w-24 h-24" />
-        <h1 className="font-bold text-center text-gray-700 tracking-wider">
-          Emaan Institute of Management and Sciences
+        <h1 className="font-bold text-left text-black ">
+          <span className="text-5xl">Emaan Institute</span>
+          <br />
+          <span className="text-2xl tracking-wide">
+            of Management and Sciences
+          </span>
         </h1>
       </div>
     </div>
