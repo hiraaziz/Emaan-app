d4f1885734db8dc8d14eb6418df8dcf7d1a08199
hiraaziz <hiraaziz1998@gmail.com>
2023-03-05 17:25:15 +0500

    Create constant.js

---
+++ src/components/footer/constant.js
@@ -0,0 +1,18 @@
+export const footernav = [
+    {
+        title: 'SERVICES',
+        navs : ['Department of Computer Science','Business Administration']
+    },
+    {
+        title: 'INFORMATION',
+        navs : ['Eligibility Criteria','Scholarship Policy','Admission Policy','Fee structure','Apply Online']
+    },
+    {
+        title: 'LEGAL',
+        navs: ['Semester Rules','Academic Calender','Academic Linkages']
+    },
+    {
+        title: 'GET IN TOUCH',
+        navs: ['info@emaan.edu.pk','hr@emaan.edu.pk','Hiring Statistics','Plot # A-2, Sector 28 Suparco Rd, Gulzar E Hijri Scheme 33, Karachi, Pakistan.']
+    }
+]
