8ad384abce37776b827a92a03809a0d98226f5fe
hiraaziz <hiraaziz1998@gmail.com>
2023-03-05 17:28:54 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,14 @@ module.exports = {
   ],
   theme: {
     fontFamily:{
-      'monserrat': ['Montserrat', 'sans-serif']
+      'monserrat': ['Montserrat', 'sans-serif'],
+      'gloock': ['Gloock', 'sans-serif'],
+      'poppins' : ['Poppins', 'sans-serif']
     },
     extend: {
       colors:{
-        'maroon': '#6C0404'
+        'maroon': '#6C0404',
+        'darkblue': '#262525'
       },
       spacing:{
         '05': '500px',
@@ -19,7 +22,8 @@ module.exports = {
         '600': '60%',
         '700': '70%',
         '800': '80%',
-        '900': '90%'
+        '900': '90%',
+        '1000': '100%'
       }
     },
   },
