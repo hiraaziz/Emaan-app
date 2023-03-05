c5e9c1905da3a5b347854becc1c9633e5a5d4863
hiraaziz <hiraaziz1998@gmail.com>
2023-03-05 17:25:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Middlebar from './components/middleBar/Middlebar';
 import Banner from './components/banner/Banner';
 import Vision from './components/vision/Vision';
 import Programs from './components/programs/Programs';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="font-montserrat">
+    <div className="font-poppins">
       <Topheader/>
       <Middlebar/>
       <Navbar/>
       <Banner/>
       <Vision/>
       <Programs/>
+      <Footer/>
     </div>
   );
 }
