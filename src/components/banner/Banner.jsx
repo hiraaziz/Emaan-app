0ec23399aa37f080a01b869eb8072eae098ab673
hiraaziz <hiraaziz1998@gmail.com>
2023-03-05 17:25:10 +0500

    Update Banner.jsx

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,13 +2,46 @@ import React from "react";
 import Crousel from "./Crousel";
 
 const Banner = () => {
-  const images = [
-    "https://picsum.photos/800/600?random=1",
-    "https://picsum.photos/800/600?random=2",
-    "https://picsum.photos/800/600?random=3",
+  const itemlist = [
+    {
+      img: "./administration.png",
+      courseName: "BBA",
+      title: "Bachleors of Business Administration",
+      length: 4,
+    },
+    {
+      img: "./commerce.png",
+      courseName: "ADP",
+      title: "Business and Commerce",
+      length: 2,
+    },
+    {
+      img: "./network.png",
+      courseName: "ADP",
+      title: "Computer Networking",
+      length: 2,
+    },
+    {
+      img: "./webdesign.png",
+      courseName: "ADP",
+      title: "Web Design and Development",
+      length: 2,
+    },
+    {
+      img: "./database.png",
+      courseName: "ADP",
+      title: "Database Management",
+      length: 2,
+    },
+    {
+      img: "./computer.png",
+      courseName: "BSCS",
+      title: "Bachleors of Science in Computer Science",
+      length: 4,
+    },
   ];
 
-  return <Crousel images={images} />;
+  return <Crousel images={itemlist} />;
 };
 
 export default Banner;
