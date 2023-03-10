ad4312496ff8e49dba64d8184cc8f3e7bd33fd0f
hiraaziz <hiraaziz1998@gmail.com>
2023-03-11 01:40:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
         <Route path="/lms" element={<Lms/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/alumni" element={<Alumni/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
       </BrowserRouter>
     </div>
