12e2e16bf8a4bda2cdc343f6722f4ab6ee824288
hiraaziz <hiraaziz1998@gmail.com>
2023-03-11 00:14:08 +0500

    Create Qec.jsx

---
+++ src/components/Qec/Qec.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "../Home/footer/Footer";
+import Middlebar from "../Home/middleBar/Middlebar";
+import Navbar from "../Home/navigationBar/Navbar";
+import Topheader from "../Home/topHeader/Topheader";
+
+const Qec = () => {
+  return (
+    <div>
+      <Topheader />
+      <Middlebar />
+      <Navbar />
+      Qec
+      <Footer />
+    </div>
+  );
+};
+
+export default Qec;
