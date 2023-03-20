c20c7e7f28e766ee0e3070197e122dfea13697b9
hiraaziz <hiraaziz1998@gmail.com>
2023-03-21 01:57:16 +0500

    Create PHPwebdevelopement.jsx

---
+++ src/components/Certificates/PHPwebdevelopement.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "../Home/footer/Footer";
+import Middlebar from "../Home/middleBar/Middlebar";
+import Navbar from "../Home/navigationBar/Navbar";
+import Topheader from "../Home/topHeader/Topheader";
+
+const PHPwebdevelopement = () => {
+  return (
+    <div>
+      <Topheader />
+      <Middlebar />
+      <Navbar />
+      PHPwebdevelopement
+      <Footer />
+    </div>
+  );
+};
+
+export default PHPwebdevelopement;
