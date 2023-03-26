c05288a5b47615c1e08fa1bd3b9e918b05457681
Rayyankkhan <126853947+Rayyankkhan@users.noreply.github.com>
2023-03-26 11:01:00 +0400

    Create Workshopform.js

---
+++ src/components/Research/ORIC/Seminars/Workshopform.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Workshopform = () => {
+  return (
+    <div>
+      <div>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfWHcmnLJgYXN41VfX807CMWgzvbd9fnBq-qinfhrqafMXbzg/viewform?embedded=true" 
+      width="640" 
+      height="2218" 
+      frameborder="0" 
+      marginheight="0" 
+      marginwidth="0">
+        Loading…
+        </iframe>
+    </div>
+    
+    </div>
+  )
+}
+
+export default Workshopform
