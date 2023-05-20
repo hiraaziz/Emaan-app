e29b61d61784be6cb5ec1adaac9ed24b645e3185
Rayyankkhan <126853947+Rayyankkhan@users.noreply.github.com>
2023-05-20 13:55:49 +0400

    Update NavMenu.jsx

--- src/components/Home/navigationBar/NavMenu.jsx
+++ src/components/Home/navigationBar/NavMenu.jsx
@@ -212,7 +212,7 @@ const NavMenu = () => {
               subDropdownItems: [
                 {
                   label: "Citation Reward Program form (CRP)",
-                  link: "/",
+                  link: "/Citation",
                 },
               ],
             },
