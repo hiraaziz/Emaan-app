83dfbe23543849377c7b8bbe6eb819540c2fb6f9
Rayyankkhan <126853947+Rayyankkhan@users.noreply.github.com>
2023-05-21 12:25:58 +0400

    Update NavMenu.jsx

--- src/components/Home/navigationBar/NavMenu.jsx
+++ src/components/Home/navigationBar/NavMenu.jsx
@@ -218,7 +218,7 @@ const NavMenu = () => {
             },
             { label: "Research Support Program ", link: "/" },
             { label: "Faculty Research Information Form", link: "/" },
-            { label: "Seminars", link: "/" },
+            { label: "Seminars", link: "/SeminarsandWorkshop" },
             { label: "Research Artical Regional", link: "/" },
           ],
         },
@@ -226,7 +226,8 @@ const NavMenu = () => {
           label: "Journals",
           hasSubDropdowns: true,
           subDropdownItems: [
-            { label: "Journal of Business and Science", link: "/" },
+            { label: "JOURNALS", link: "/JournalsResearch" },
+            { label: "Journal of Business and Science", link: "/EJMS" },
             { label: "Journal of Computer Science", link: "/" },
           ],
         },
